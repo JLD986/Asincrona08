8f6caf94d7fcd05bc195d0dd11f4c751d662bbd7
JLD986 <126208860+JLD986@users.noreply.github.com>
2023-03-18 18:13:12 -0600

    Update actividadAsincrona – Semana08(Grupo9).py

--- actividadAsincrona – Semana08(Grupo9).py
+++ actividadAsincrona – Semana08(Grupo9).py
@@ -11,28 +11,28 @@ opción = int(input("¿Cuál es la opción que desea seleccionar? \n"))  #Esta l
 if opción == 1:                               #Aqui empieza la opcion uno si la variable es igual a uno de los elif o if se ejecutara lo planteado en cada linea
     print("Identificador de gentilicios\n")
     
-    opcionhumano = str(input("¿Cuál es el gentilicio que desea identificar? "))
+    opcionhumano = str(input("¿Cuál es el gentilicio que desea identificar? ")).lower()
     
-    if opcionhumano == "El Salvador":
-        print("Su gentilicio es 'salvadoreño' ")
-    elif opcionhumano == "Mexico":
-        print("Su gentilicio es 'mexicano/a' ")
-    elif opcionhumano == "Estados Unidos":
-        print("Su gentilicio es 'estadounidense' ")
-    elif opcionhumano == "Canada":
-        print("Su gentilicio es 'canadiense' ")
-    elif opcionhumano == "Inglaterra":
-        print("Su gentilico es 'británico/a' ")
-    elif opcionhumano == "Argentina":
-        print("Su gentilicio es 'argentino/a' ")
-    elif opcionhumano == "España":
-        print("Su gentilicio es 'español/a' ")
-    elif opcionhumano == "Chile":
-        print("SU gentilicio es 'chileno/a' ")
-    elif opcionhumano == "Australia":
-        print("Su gentilicio es 'australiano/a' ")
-    elif opcionhumano == "Italia":
-        print("Su gentilicio es 'italiano/a' ")          #Aqui terminan los elif
+    if opcionhumano == "el salvador":
+        print("Su gentilicio es 'Salvadoreño' ")
+    elif opcionhumano == "mexico":
+        print("Su gentilicio es 'Mexicano/a' ")
+    elif opcionhumano == "estados unidos":
+        print("Su gentilicio es 'Estadounidense' ")
+    elif opcionhumano == "canada":
+        print("Su gentilicio es 'Canadiense' ")
+    elif opcionhumano == "inglaterra":
+        print("Su gentilico es 'Británico/a' ")
+    elif opcionhumano == "argentina":
+        print("Su gentilicio es 'Argentino/a' ")
+    elif opcionhumano == "españa":
+        print("Su gentilicio es 'Español/a' ")
+    elif opcionhumano == "chile":
+        print("SU gentilicio es 'Chileno/a' ")
+    elif opcionhumano == "australia":
+        print("Su gentilicio es 'Australiano/a' ")
+    elif opcionhumano == "italia":
+        print("Su gentilicio es 'Italiano/a' ")          #Aqui terminan los elif
     else:                                                 #La opcion else por si ningun dato introducido es igual
         print('Ese no esta en la base de datos \n')           
         
@@ -40,27 +40,27 @@ if opción == 1:                               #Aqui empieza la opcion uno si la
 elif opción == 2:                                         #Aqui empieza la opcion identificador de Animales, si la variable introducida es igual a alguno de los if se desarrollara correctamente
     print("Identificador de tipo de animal")
     
-    opcionanimal = str(input("¿Cuál es el tipo de animal que desea idetificar?"))        #Aqui introduce el nombre de un animal
+    opcionanimal = str(input("¿Cuál es el tipo de animal que desea idetificar?")).lower()        #Aqui introduce el nombre de un animal
     
-    if opcionanimal == "Lobo":
+    if opcionanimal == "lobo":
         print("El tipo de animal es 'canino'")
-    elif opcionanimal == "Tigre":
+    elif opcionanimal == "tigre":
         print("El tipo de animal es 'felino'")
-    elif opcionanimal == "Vaca":
+    elif opcionanimal == "vaca":
         print("El tipo de animal es 'bobino'")
-    elif opcionanimal == "Caballo":
+    elif opcionanimal == "caballo":
         print("El tipo de animal es 'equino'")
-    elif opcionanimal == "Oso":
+    elif opcionanimal == "oso":
         print("El tipo de animal es 'pantígrado'")
-    elif opcionanimal == "Colibrí":
+    elif opcionanimal == "colibrí":
         print("El tipo de animal es 'ave'")
-    elif opcionanimal == "Iguana":
+    elif opcionanimal == "iguana":
         print("El tipo de animal es 'reptil'")
-    elif opcionanimal == "Perro":
+    elif opcionanimal == "perro":
         print("El tipo de animal es 'canino'")
-    elif opcionanimal == "Pingüino":
+    elif opcionanimal == "pingüino":
         print("El tipo de animal es 'ave'")
-    elif opcionanimal == "León":
+    elif opcionanimal == "leon":   #sin tilde
         print("El tipo de animal es 'felino'")      #Aqui terminan todas las opciones de if que habias
     else:                                          #Y el else por si la variable no cuadra con ningun if
         print("El animal ingresado no se puede identificar")
